1968718e4b3c81d467c3a0e29f100baa9bb9b695
BishalS422 <sbishal422@gmail.com>
2021-12-17 00:03:56 +0545

    newCommite

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
-function App() {
-  return <div>hello</div>;
+import PropTypes from "prop-types";
+
+function App({ bgColor, textColor, displatFlex, text, center }) {
+  const headerStyle = {
+    backgroundColor: bgColor,
+    color: textColor,
+    display: displatFlex,
+    alignItems: "center",
+    justifyContent: center,
+  };
+  return (
+    <div>
+      <h1 style={headerStyle}>{text}</h1>
+    </div>
+  );
 }
+App.defaultProps = {
+  text: "Feedback APP",
+  bgColor: "#42968c",
+  textColor: "#eef0f0",
+  displatFlex: "flex",
+  center: "center",
+};
+
+App.propTypes = {
+  bgColor: PropTypes.string,
+};
 
 export default App;
