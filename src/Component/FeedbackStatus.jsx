06076d3239831952cf9823fe3ac2641be6b79073
BishalS422 <sbishal422@gmail.com>
2021-12-23 13:53:00 +0545

    add reduce function to add rating

--- src/Component/FeedbackStatus.jsx
+++ src/Component/FeedbackStatus.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 
 function FeedbackStatus({ feedback }) {
-  let sum = 0;
+  // hear we use reduce fuction to add rating of all feedback
+  const sumRating = feedback.reduce(
+    (total, feedback) => total + feedback.rating,
+    0
+  );
+  console.log(sumRating);
 
-  for (let value of feedback) {
-    sum = value.rating + value.rating;
-  }
-
-  const average = (sum / feedback.length).toPrecision(3);
+  const average = (sumRating / feedback.length).toPrecision(3);
 
   return (
     <div>
       <h2> {feedback.length} Review</h2>
+
       <h2> Average Review:{average}</h2>
     </div>
   );
